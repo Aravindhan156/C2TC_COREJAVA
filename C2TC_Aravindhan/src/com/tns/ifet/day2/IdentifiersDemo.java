9ad2bc3afc4b3b0fe55bacd06072d42ee1fba4ab
DELL <DELL@AravindhBosss>
2024-10-27 19:05:58 +0530

    FirstCommit

---
+++ C2TC_Aravindhan/src/com/tns/ifet/day2/IdentifiersDemo.java
@@ -0,0 +1,8 @@
+public class IdentifersDemo {
+public static void main(String[] args) {
+int $number = 20; 
+System.out.println("value of the numbervariable is : "+ $number);
+String studentName="Aniket"; 
+System.out.println("value is : "+ studentName);
+		}
+		}
